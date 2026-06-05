b87861e57c9d2fcf51227bd511c86013793f2cfc
Serg <37512907+Serg4nt-web@users.noreply.github.com>
2026-06-05 09:40:19 +0800

    Delete security-preview.js

--- security-preview.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const TARGET_TIER = "Dev_QA_Elite";
-let userWallet = prompt("Введите адрес кошелька Base Eco");
-let isAgreed = confirm(`Вы согласны с условиями транзакции?\n${userWallet}`);
-alert(isAgreed);
